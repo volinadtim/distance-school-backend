5a1e9cda270c788ba473cb28829eca689b8fd7d3
Danilov Timofey <volinadtim@gmail.com>
2020-04-24 08:32:43 +0300

    restructure

--- distance_school/urls.py
+++ distance_school/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from distance_school.back_school.views import RegView
+from back_school.views import RegView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
